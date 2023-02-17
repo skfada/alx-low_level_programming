cc0278e045ee77d1d2f911a9894e9d6c68c0fd3b
skfada <adamusolomon44@gmail.com>
2023-02-17 11:57:52 -0800

    using the if and else statement

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,26 +2,32 @@
 #include <time.h>
 /* more headers goes there */
 #include <stdio.h>
+/* betty style doc for function main goes there */
 /**
  *  * main - assigns a random number to int n everytime
  *   *it executes, and prints it
  *    *
  *     * Return: Always 0 (Success)
- *      *//* betty style doc for function main goes there */
+ *      */
 int main(void)
 {
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-/* your code goes there */
-if(n > 0){
-printf("%d is positive\n",n);
-}
-else if (n == 0){
-printf("%d is zero\n",n);
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+
+	if (n > 0)
+	{
+		printf("%d is positive\n",n);
 	}
-else{
-printf("%d is negative\n",n);
-}
-return (0);
+	else if (n == 0)
+	{
+		printf("%d is zero\n",n);
+	}
+	else
+	{
+		printf("%d is negative\n",n);
+	}
+	return (0);
 }
