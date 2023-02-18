73fa5b67a5b1a6c12be0576fc788c4aeecc07f03
skfada <adamusolomon44@gmail.com>
2023-02-17 16:38:01 -0800

    using the if and else statement

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints the alphabet in lowercase, and then in uppercase,
+ * main - prints the alphabet in lowercase,
  * followed by a new line
  *
  * Return: Always 0 (Success)
@@ -11,17 +11,17 @@ int main(void)
 
 {
 
-int ch;
+char ch;
 
 for (ch = 'a'; ch <= 'z'; ch++)
 
+{
 putchar(ch);
 
-for (ch = 'A'; ch <= 'Z'; ch++)
-
-putchar(ch);
+}
 
 putchar('\n');
 
-return (0);
+ return (0);
+
 }
