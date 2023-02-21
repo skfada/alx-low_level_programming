587f006910fdb6a6642871d6a53993e819944b89
skfada <adamusolomon44@gmail.com>
2023-02-21 19:21:07 +0100

    program that prints _putchar, followed by a new line.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,7 @@
  * main - this is the main funtion
  * parameter : the main receives no parameter
  * the main function will print "_putchar\n"
- * return : and will return Zero value
+ * Return: and will return Zero value
  */
 int main(void)
 {
