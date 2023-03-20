56a4e6521cf6f2df4eb85358ffb9ffee41016515
skfada <adamusolomon44@gmail.com>
2023-03-20 10:35:40 +0100

    type struct dog with the following elements

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+ This project is on 0x0E. C - Structures, typedef
---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,11 @@
+fndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+
+#endiffndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+
+#endif
