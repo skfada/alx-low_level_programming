4f4d0ee66393d4909de436daf1f093bc449cbe3f
skfada <adamusolomon44@gmail.com>
2023-05-10 10:23:25 +0100

    update

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,8 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <elf.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
